00a50cca12023e3c4c63f6a32f2db39f3d6eff3c
ErnestUrzua <ernieurzua@gmail.com>
2020-06-18 02:53:03 -0700

    added special character array

--- script.js
+++ script.js
@@ -41,6 +41,7 @@ if(lengthValid === true) {
   function generatePassword(u,l,s);
   var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","Q","Y","Z"];  
   var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","q","y","z"];
+  var special = ["1","2","3","4","5","6","7","8","9","0","!","@","#","$","%","^","&","*"];
   //generate random upercase letters
       //go thru array of letters
       //use math.random in for loop to select each index up to 30
