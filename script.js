fdfa7d3a2a125f858214a747c7c049b43b0d3c97
ErnestUrzua <ernieurzua@gmail.com>
2020-06-18 02:03:46 -0700

    validation function to end alerts after invalid input

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+var lengthValid = false;
+
 //length validation function
 function lengthValidation(x) {
 
   // If length is Not a Number or less than 8 or greater than 128
   if (isNaN(x) || x < 8 || x > 128) {
     alert("Input not valid, please try again");
-   // location.reload();
+    
   } 
   else {
+    lengthValid = true;
     //do nothing proceed
   }
 }
@@ -20,28 +23,32 @@ var length = prompt("Please choose a password length between 8 and 128 character
 //call length function to validate length of string 
 lengthValidation(length);
 
-//add in prompt for password that is "response" length with at lease 1 uppercase, 1 lowercase , 
-alert("you must select at least 1 character type uppercase, lowercase, and special characters in order to be a valid password.");
-//ask user if they want to use uppercase character type
-var uppercase = confirm("Do you want to include UPPERCASE characters?");
+if(lengthValid === true) {
 
-//ask user if they want to use lowercase characters
-var lowercase = confirm("Do you want to include lowercase characters?");
+  //add in prompt for password that is "response" length with at lease 1 uppercase, 1 lowercase , 
+  alert("you must select at least 1 character type uppercase, lowercase, and special characters in order to be a valid password.");
+  //ask user if they want to use uppercase character type
+  var uppercase = confirm("Do you want to include UPPERCASE characters?");
 
-var specialCharacter = confirm("Do you want to include special characters?")
+  //ask user if they want to use lowercase characters
+  var lowercase = confirm("Do you want to include lowercase characters?");
+
+  var specialCharacter = confirm("Do you want to include special characters?")
 
 
-//validate the users character selections to include at least 1 of the 3 character types
+  //validate the users character selections to include at least 1 of the 3 character types
 
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
+
+  }
+
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
