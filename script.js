1a06667c93ec49a159ba2ee38c06292c5408c65d
LoneStar <ernieurzua@gmail.com>
2020-06-18 19:14:33 -0700

    final array password outputs to variable password string

--- script.js
+++ script.js
@@ -25,11 +25,12 @@ lengthValidation(length);
 
 if(lengthValid === true) {
 
+  //add this to the html page instead of alert
   //add in prompt for password that is "response" length with at lease 1 uppercase, 1 lowercase , 
   alert("you must select at least 1 character type uppercase, lowercase, and special characters in order to be a valid password.");
   
   //ask user if they want to use uppercase character type
-  var uppercase = confirm("Do you want to include UPPERCASE characters?");
+  var uppercase = confirm("Do you want to include uppercase characters?");
 
   //ask user if they want to use lowercase characters
   var lowercase = confirm("Do you want to include lowercase characters?");
@@ -38,65 +39,70 @@ if(lengthValid === true) {
   var specialCharacter = confirm("Do you want to include special characters?")
 
   //ask if they want number characters
-  var numberCharacter = confirm("Do you want to include special characters?")
+  var numberCharacter = confirm("Do you want to include number characters?")
 
   //validate at least 1  type of character is present that user specified
   //CODE HERE//
 
-  function generatePassword(upercase,lowercase,specialCharacter,numbers);
+  function generatePassword(){
     var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","Q","Y","Z"];  
     var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","q","y","z"];
     var special = ["!","@","#","$","%","^","&","*","(",")","_","+"];
     var numbers = ["1","2","3","4","5","6","7","8","9"];
-    var count = 0;  
-    var final = [];
-    var x;
+    //var count = 0;  
+    var final = []; //array to hold final string 
+    var password;
 
     //for loop of user specified length
     for (var i = 0;i<length;i++){
-
-        //for loop that iterates through all cases 
-        for (var j = 0;j<length;j++){
 
           if (uppercase === true) {
             //take a random index from upperCase[]
             x = Math.floor((Math.random() * upperCase.length));
             final.push(upperCase[x]);
+            // console.log(final[x]);
           }
 
-          if (lowerCase === true) {
+          if (lowercase === true) {
             //take a random index from lowerCase
             x = Math.floor((Math.random() * lowerCase.length));
             final.push(lowerCase[x]);
+            // console.log(final[x]);
           }
 
-          if (special === true) {
+          if (specialCharacter === true) {
             //take a random index from lowerCase
             x = Math.floor((Math.random() * special.length));
             final.push(special[x]);
+            // console.log(final[x]);
           }
           
-          if (numbers === true) {
+          if (numberCharacter === true) {
             //take a random index from lowerCase
             x = Math.floor((Math.random() * numbers.length));
             final.push(numbers[x]);
+            // console.log(final[x]);
           }
-        }
+       password = final.join();//put final array into password
+       
     }
-   
+  }
+  
 
-
+  
     
 
   // Write password to the password box
   function writePassword() {
-    var password = generatePassword();
+    generatePassword();
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
+    console.log(password);
    
   }
 
+
   // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+  generateBtn.addEventListener("click", writePassword());
 
 }
